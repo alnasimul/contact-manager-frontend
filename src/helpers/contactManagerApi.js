220b9e8decb3d143f038a42271b191b85b4147cf
alnasimul <alnasimul@gmail.com>
2023-01-28 06:00:35 +0600

    another commit

--- src/helpers/contactManagerApi.js
+++ src/helpers/contactManagerApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://contact-manager-api-k6r8.onrender.com'
-   // baseURL: 'https://serene-peak-42468.herokuapp.com'
+   // baseURL: 'http://localhost:4000'
 })
